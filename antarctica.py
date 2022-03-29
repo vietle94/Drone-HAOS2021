d6182f77cde1748f8ccf65b48dcb7ccc3d85138d
Viet Le (FMI) <viet.le@fmi.fi>
2022-03-29 13:12:32 +0300

    merged ref and wind, only smooth the pressure

--- antarctica.py
+++ antarctica.py
@@ -11,6 +11,8 @@ import os
 import fnmatch
 import xarray as xr
 import pywt
+from sklearn.metrics import r2_score
+
 %matplotlib qt
 
 # %%
@@ -61,41 +63,102 @@ for x, x_name in zip(file_path, file_names):
         plt.close()
 
 # %%
-
-ref = pd.read_csv(base_dir + r'mavic_Matt\weather.csv')
-
-# %%
-df = pd.read_csv(
-    r'C:\Users\vietl\Downloads\Antarctica\Antarctica\mavic_Matt\mavic_profiles_gridded/mavic_profile_gridded_20211211_20.55.csv')
-weather = pd.read_csv(r'C:\Users\vietl\Downloads\Antarctica\Antarctica\mavic_Matt/weather.csv')
+ref = pd.DataFrame({})
+weather_full = pd.read_csv(base_dir + r'mavic_Matt\weather.csv')
+weather_full = weather_full.rename({'date_time': 'datetime'}, axis='columns')
+weather_full.datetime = pd.to_datetime(weather_full.datetime)
 
 # %%
-df.date_time = pd.to_datetime(df.date_time)
-weather.date_time = pd.to_datetime(weather.date_time)
+for file in glob.glob(base_dir + r'Viet\merged_wind/*.csv'):
+    df_name = file.split('\\')[-1][:-4]
+    df = pd.read_csv(file)
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df[['datetime', 'press_bme']] = df[['datetime', 'press_bme']].set_index(
+        'datetime').rolling('10s').median().reset_index()
+    weather = weather_full[weather_full.datetime.dt.date == df['datetime'][0].date()]
 
-weather = weather[weather.date_time.dt.date == df['date_time'][0].date()]
+    temp = df.merge(weather, on='datetime', how='left')
+    temp[['Home Distance', 'Wind Direction',
+          'Wind Speed']] = temp[['Home Distance', 'Wind Direction',
+                                 'Wind Speed']].fillna(method='backfill', limit=10)
+    temp[['wdir_3s', 'wdir_10m', 'rh_1m', 'press_1m', 'press_surf_1m', 'glob_rad',
+          'temp_1m', 'dewpoint', 'ws_3s', 'ws_10m', 'ws_gust',
+          'voltage']] = temp[['wdir_3s', 'wdir_10m', 'rh_1m', 'press_1m', 'press_surf_1m', 'glob_rad',
+                              'temp_1m', 'dewpoint', 'ws_3s', 'ws_10m', 'ws_gust',
+                              'voltage']].fillna(method='backfill', limit=60)
+
+    intersec = temp[np.abs(temp.press_1m - temp.press_bme) < 0.3]
+    intersec = intersec.reset_index(drop=True)
+    ref = ref.append(intersec)
+
+    fig, ax = plt.subplots(3, 2, sharex=True, figsize=(16, 9))
+
+    ax[0, 0].plot(temp.datetime, temp.press_bme, '.', label='press_bme')
+    ax[0, 0].plot(temp.datetime, temp.press_1m, '.', label='press_1m_tower')
+    ax[0, 0].set_ylabel('Pressure [hPa]')
+
+    ax[1, 0].plot(temp.datetime, temp.temp_bme, '.', label='temp_bme')
+    ax[1, 0].plot(temp.datetime, temp.temp_1m, '.', label='temp_1m_tower')
+    ax[1, 0].set_ylabel('Temperature [$^0C$]')
+
+    ax[2, 0].plot(temp.datetime, temp['Wind Speed'], '.', label='Wind speed')
+    ax[2, 0].plot(temp.datetime, temp['ws_3s'], '.', label='wind_3m_tower')
+    ax[2, 0].set_ylabel('Wind Speed [m/s]')
+
+    ax[0, 1].plot(temp.datetime, temp.Altitude, '.', label='Drone Altitude from wind')
+    ax[0, 1].set_ylabel('Altitude [m]')
+
+    ax[1, 1].plot(temp.datetime, temp.rh_bme, '.', label='rh_bme')
+    ax[1, 1].plot(temp.datetime, temp.rh_1m, '.', label='rh_1m_tower')
+    ax[1, 1].set_ylabel('Relative humidity [%]')
+
+    ax[2, 1].plot(temp.datetime, temp['Wind Direction'], '.', label='Wind direction')
+    ax[2, 1].plot(temp.datetime, temp['wdir_3s'], '.', label='wind_direction_3m_tower')
+    ax[2, 1].set_ylabel('Wind direction [$^0$]')
+
+    for ax_ in ax.flatten():
+        ax_.legend()
+        ax_.grid()
+
+    for vline in intersec.datetime:
+        for ax_ in ax.flatten():
+            ax_.axvline(x=vline)
+    fig.suptitle(df_name)
+    fig.savefig(dir_save + 'plots/' + df_name + '_full.png', bbox_inches='tight')
+    plt.close()
 
 # %%
-df = df.set_index('date_time').resample('10s').median().reset_index()
-weather = weather.set_index('date_time').resample('10s').bfill().reset_index()
-
-temp = df.merge(weather, on='date_time')
-temp
+ref.to_csv(base_dir + r'Viet\ref_compare.csv', index=False)
 
 # %%
-fig, ax = plt.subplots(2, 1)
-ax[0].plot(temp.date_time, temp.press_bme)
-ax[0].plot(temp.date_time, temp.press_1m, '.')
-ax[1].plot(temp.date_time, temp.temp_bme)
-ax[1].plot(temp.date_time, temp.temp_1m, '.')
-# %%
-fig, ax = plt.subplots()
-ax.plot(temp.date_time, temp.press_1m - temp.press_bme)
+fig, ax = plt.subplots(3, 2, figsize=(16, 9))
+for ax_, x, y in zip(ax.flatten(),
+                     ['wdir_3s', 'ws_3s', 'temp_1m', 'rh_1m',
+                      'press_1m'],
+                     ['Wind Direction', 'Wind Speed', 'temp_bme', 'rh_bme',
+                      'press_bme']
+                     ):
 
-# %%
-temp[np.abs(temp.press_1m - temp.press_bme) < 0.2]
+    ax_.plot(ref[x], ref[y], '.')
+    ax_.set_xlabel(x)
+    ax_.set_ylabel(y)
+    axline_min = np.min((np.min(ref[x]), np.min(ref[y])))
+    axline_max = np.min((np.max(ref[x]), np.max(ref[y])))
+    ax_.axline((axline_min, axline_min),
+               (axline_max, axline_max),
+               color='grey', linewidth=0.5, ls='--')
+    try:
+        line = ref[[x, y]].copy()
+        line = line.dropna()
+        z = np.polyfit(line[x],
+                       line[y], 1)
+    except Exception:
+        continue
+    y_hat = np.poly1d(z)(line[x])
+    ax_.plot(line[x], y_hat)
+    text = f"$y={z[0]:0.3f}\;x{z[1]:+0.3f}$\n$R^2 = {r2_score(line[y], y_hat):0.3f}$"
+    ax_.text(0.05, 0.95, text, transform=ax_.transAxes,
+             fontsize=10, verticalalignment='top')
 
-# %%
-fig, ax = plt.subplots()
-ax.plot(temp.date_time, temp.temp_bme)
-ax.plot(temp.date_time, temp.temp_1m, '.')
+fig.subplots_adjust(hspace=0.3)
+fig.savefig(base_dir + r'Viet\ref_compare.png', bbox_inches='tight')
