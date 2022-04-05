9f563d283ed107cb8aa36b68a3293d4c900b8e20
Viet Le (FMI) <viet.le@fmi.fi>
2022-04-05 15:38:56 +0300

    add no need wind data to compare with ref

--- antarctica.py
+++ antarctica.py
@@ -27,7 +27,9 @@ file_names_wind = [os.path.basename(x) for x in glob.glob(dir_wind + '*.csv')]
 
 dir_save = base_dir + r'Viet\merged_wind/'
 
+################################
 # %%
+################################
 appended_df_wind = []
 for x, x_name in zip(file_path_wind, file_names_wind):
     df_temp = pd.read_csv(x)
@@ -162,3 +164,68 @@ for ax_, x, y in zip(ax.flatten(),
 
 fig.subplots_adjust(hspace=0.3)
 fig.savefig(base_dir + r'Viet\ref_compare.png', bbox_inches='tight')
+
+#########################################
+# %% No need wind data
+#########################################
+ref = pd.DataFrame({})
+weather_full = pd.read_csv(base_dir + r'mavic_Matt\weather.csv')
+weather_full = weather_full.rename({'date_time': 'datetime'}, axis='columns')
+weather_full.datetime = pd.to_datetime(weather_full.datetime)
+
+# %%
+for file in file_path:
+    df_name = file.split('\\')[-1][:-4]
+    df = pd.read_csv(file)
+    df.dropna(how='all', inplace=True)
+    df = df.rename({'date_time': 'datetime'}, axis='columns')
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df[['datetime', 'press_bme']] = df[['datetime', 'press_bme']].set_index(
+        'datetime').rolling('10s').median().reset_index()
+
+    temp = df.merge(weather_full, on='datetime', how='left')
+    temp[['wdir_3s', 'wdir_10m', 'rh_1m', 'press_1m', 'press_surf_1m', 'glob_rad',
+          'temp_1m', 'dewpoint', 'ws_3s', 'ws_10m', 'ws_gust',
+          'voltage']] = temp[['wdir_3s', 'wdir_10m', 'rh_1m', 'press_1m', 'press_surf_1m', 'glob_rad',
+                              'temp_1m', 'dewpoint', 'ws_3s', 'ws_10m', 'ws_gust',
+                              'voltage']].fillna(method='backfill', limit=60)
+
+    intersec = temp[np.abs(temp.press_1m - temp.press_bme) < 0.3]
+    intersec = intersec.reset_index(drop=True)
+    ref = ref.append(intersec)
+
+# %%
+ref.columns
+
+# %%
+fig, ax = plt.subplots(2, 2, figsize=(16, 9))
+for ax_, x, y in zip(ax.flatten(),
+                     ['temp_1m', 'rh_1m',
+                      'press_1m'],
+                     ['temp_bme', 'rh_bme',
+                      'press_bme']
+                     ):
+
+    ax_.plot(ref[x], ref[y], '.')
+    ax_.set_xlabel(x)
+    ax_.set_ylabel(y)
+    axline_min = np.min((np.min(ref[x]), np.min(ref[y])))
+    axline_max = np.min((np.max(ref[x]), np.max(ref[y])))
+    ax_.axline((axline_min, axline_min),
+               (axline_max, axline_max),
+               color='grey', linewidth=0.5, ls='--')
+    try:
+        line = ref[[x, y]].copy()
+        line = line.dropna()
+        z = np.polyfit(line[x],
+                       line[y], 1)
+    except Exception:
+        continue
+    y_hat = np.poly1d(z)(line[x])
+    ax_.plot(line[x], y_hat)
+    text = f"$y={z[0]:0.3f}\;x{z[1]:+0.3f}$\n$R^2 = {r2_score(line[y], y_hat):0.3f}$"
+    ax_.text(0.05, 0.95, text, transform=ax_.transAxes,
+             fontsize=10, verticalalignment='top')
+
+fig.subplots_adjust(hspace=0.3)
+fig.savefig(base_dir + r'Viet\ref_compare_all.png', bbox_inches='tight')
