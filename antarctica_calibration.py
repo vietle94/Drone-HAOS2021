89983326beaa4a60a670e12787c1d1de11da559d
Viet Le (FMI) <viet.le@fmi.fi>
2022-05-12 14:42:02 +0300

    select only good data

--- antarctica_calibration.py
+++ antarctica_calibration.py
@@ -1,3 +1,5 @@
+import matplotlib.dates as mdates
+from matplotlib.widgets import RectangleSelector, SpanSelector
 import numpy as np
 from sklearn.cluster import DBSCAN
 from functools import reduce
@@ -29,8 +31,6 @@ dir_wind = base_dir + r'/Calibration_flights/Wind_estimates/'
 file_path_wind = [x for x in glob.glob(dir_wind + '*.csv')]
 file_names_wind = [os.path.basename(x) for x in glob.glob(dir_wind + '*.csv')]
 
-dir_save = base_dir + r'Viet\merged_wind/'
-
 appended_df_wind = []
 for x, x_name in zip(file_path_wind, file_names_wind):
     df_temp = pd.read_csv(x)
@@ -48,84 +48,21 @@ weather_full = pd.read_csv(base_dir + r'mavic_Matt\weather.csv')
 weather_full = weather_full.rename({'date_time': 'datetime'}, axis='columns')
 weather_full.datetime = pd.to_datetime(weather_full.datetime)
 weather_full = weather_full.drop(['wdir_10m', 'ws_10m'], axis=1)
-weather_full['press_1m_at11m'] = weather_full['press_1m'] + 0.7
 
 # %%
-for x, x_name in zip(file_path, file_names):
-    df = pd.read_csv(x)
-    df.dropna(how='all', inplace=True)
-    i_min = np.argmin(df.press_bme)
-    df['ascend'] = True
-    df.loc[i_min:, 'ascend'] = False
-    df = df.rename({'date_time': 'datetime'}, axis='columns')
-    df['datetime'] = pd.to_datetime(df['datetime'])
-    df = df.reset_index(drop=True)
 
-    df_full = df.merge(df_wind, on='datetime', how='left')
-    df_full = df_full.merge(weather_full, on='datetime', how='left')
-
-    df_full[['wdir_3s', 'rh_1m', 'press_1m', 'press_surf_1m', 'glob_rad',
-             'temp_1m', 'dewpoint', 'ws_3s', 'ws_gust',
-             'voltage']] = df_full[['wdir_3s', 'rh_1m', 'press_1m', 'press_surf_1m', 'glob_rad',
-                                    'temp_1m', 'dewpoint', 'ws_3s', 'ws_gust',
-                                    'voltage']].fillna(method='backfill', limit=60)
-    df_full[['Altitude', 'Home Distance',
-             'Wind Direction', 'Wind Speed']] = df_full[['Altitude', 'Home Distance',
-                                                         'Wind Direction', 'Wind Speed']].fillna(method='ffill', limit=10)
-
-    fig, ax = plt.subplots(4, 2, sharex=True, figsize=(16, 9))
-
-    ax[0, 0].plot(df_full.datetime, df_full.press_bme, '.', label='press_bme')
-    ax[0, 0].plot(df_full.datetime, df_full.press_1m, '.', label='press_1m_tower')
-    ax[0, 0].set_ylabel('Pressure [hPa]')
-
-    ax[0, 1].plot(df_full.datetime, df_full.Altitude, '.', label='Drone Altitude from wind')
-    ax[0, 1].set_ylabel('Altitude [m]')
-
-    ax[1, 0].plot(df_full.datetime, df_full.temp_bme, '.', label='temp_bme')
-    ax[1, 0].plot(df_full.datetime, df_full.temp_1m, '.', label='temp_1m_tower')
-    ax[1, 0].set_ylabel('Temperature [$^0C$]')
-
-    ax[1, 1].plot(df_full.datetime, df_full.rh_bme, '.', label='rh_bme')
-    ax[1, 1].plot(df_full.datetime, df_full.rh_1m, '.', label='rh_1m_tower')
-    ax[1, 1].set_ylabel('Relative humidity [%]')
-
-    ax[2, 0].plot(df_full.datetime, df_full['temp_sht'], '.', label='temp_sht')
-    ax[2, 0].plot(df_full.datetime, df_full['temp_1m'], '.', label='temp_1m_tower')
-    ax[2, 0].set_ylabel('Temperature [$^0C$]')
-
-    ax[2, 1].plot(df_full.datetime, df_full['rh_sht'], '.', label='rh_sht')
-    ax[2, 1].plot(df_full.datetime, df_full['rh_1m'], '.', label='rh_1m_tower')
-    ax[2, 1].set_ylabel('Relative humidity [%]')
-
-    ax[3, 0].plot(df_full.datetime, df_full['Wind Speed'], '.', label='Wind speed')
-    ax[3, 0].plot(df_full.datetime, df_full['ws_3s'], '.', label='wind_3m_tower')
-    ax[3, 0].set_ylabel('Wind Speed [m/s]')
-
-    ax[3, 1].plot(df_full.datetime, df_full['Wind Direction'], '.', label='Wind direction')
-    ax[3, 1].plot(df_full.datetime, df_full['wdir_3s'], '.', label='wind_direction_3m_tower')
-    ax[3, 1].set_ylabel('Wind direction [$^0$]')
-
-    for ax_ in ax.flatten():
-        ax_.legend()
-        ax_.grid()
-
-    fig.savefig(base_dir + r'Viet/cal_flights/' + x_name + '_timeseries.png',
-                bbox_inches='tight')
-
-# %%
 df = pd.DataFrame({})
-for x in file_path:
+for i, x in enumerate(file_path):
     df_ = pd.read_csv(x)
+    df_['flight_ID'] = i
     df_.dropna(how='all', inplace=True)
-    i_min = np.argmin(df_.press_bme)
-    df_['ascend'] = True
-    df_.loc[i_min:, 'ascend'] = False
     df = df.append(df_)
+# df = pd.concat([pd.read_csv(x) for x in file_path], ignore_index=True)
 df = df.rename({'date_time': 'datetime'}, axis='columns')
 df['datetime'] = pd.to_datetime(df['datetime'])
 df = df.reset_index(drop=True)
 
+# %%
 df_full = df.merge(df_wind, on='datetime', how='left')
 df_full = df_full.merge(weather_full, on='datetime', how='left')
 
@@ -137,9 +74,86 @@ df_full[['wdir_3s', 'rh_1m', 'press_1m', 'press_surf_1m', 'glob_rad',
 df_full[['Altitude', 'Home Distance',
          'Wind Direction', 'Wind Speed']] = df_full[['Altitude', 'Home Distance',
                                                      'Wind Direction', 'Wind Speed']].fillna(method='ffill', limit=10)
+df_full = df_full[df_full.datetime.dt.year != 1970]
+df_full = df_full.reset_index(drop=True)
+df_full = df_full[~pd.isnull(df_full['datetime'])]
 
 # %%
-data_plot = df_full
+
+
+class span_select():
+
+    def __init__(self, x, y, ax_in, canvas, orient='horizontal'):
+        self.x, self.y = x, y
+        self.ax_in = ax_in
+        self.canvas = canvas
+        self.selector = SpanSelector(
+            self.ax_in, self, orient,
+            span_stays=True, useblit=True
+        )
+
+    def __call__(self, min, max):
+        min = mdates.num2date(min).replace(tzinfo=None)
+        max = mdates.num2date(max).replace(tzinfo=None)
+        print(f'from {min} to {max}')
+        self.min, self.max = min, max
+        self.maskx = (self.x > min) & (self.x < max)
+        self.selected_x = self.x[self.maskx]
+        self.selected_y = self.y[self.maskx]
+
+
+# %%
+df_full['quality_flag'] = 0
+# df_filtered = pd.DataFrame({})
+i = -1
+
+# %%
+i += 1
+grp = df_full[df_full['flight_ID'] == i]
+
+fig2, ax = plt.subplots(3, 2, figsize=(16, 9), sharex=True)
+ax[0, 0].plot(grp.datetime, grp.press_bme, '.', label='press_bme')
+ax[0, 0].plot(grp.datetime, grp.press_1m, '.', label='press_1m_tower')
+ax[0, 0].set_ylabel('Pressure [hPa]')
+
+ax[0, 1].plot(grp.datetime, grp.Altitude, '.', label='Drone Altitude from wind')
+ax[0, 1].set_ylabel('Altitude [m]')
+
+ax[1, 0].plot(grp.datetime, grp.temp_bme, '.', label='temp_bme')
+ax[1, 0].plot(grp.datetime, grp.temp_sht, '.', label='temp_sht')
+ax[1, 0].plot(grp.datetime, grp.temp_1m, '.', label='temp_1m_tower')
+ax[1, 0].set_ylabel('Temperature [$^0C$]')
+
+ax[1, 1].plot(grp.datetime, grp.rh_bme, '.', label='rh_bme')
+ax[1, 1].plot(grp.datetime, grp.rh_sht, '.', label='rh_sht')
+ax[1, 1].plot(grp.datetime, grp.rh_1m, '.', label='rh_1m_tower')
+ax[1, 1].set_ylabel('Relative humidity [%]')
+
+ax[2, 0].plot(grp.datetime, grp['Wind Speed'], '.', label='Wind speed')
+ax[2, 0].plot(grp.datetime, grp['ws_3s'], '.', label='wind_3s_tower')
+ax[2, 0].set_ylabel('Wind Speed [m/s]')
+
+ax[2, 1].plot(grp.datetime, grp['Wind Direction'], '.', label='Wind direction')
+ax[2, 1].plot(grp.datetime, grp['wdir_3s'], '.', label='wind_direction_3s_tower')
+ax[2, 1].set_ylabel('Wind direction [$^0$]')
+
+for ax_ in ax.flatten():
+    ax_.legend()
+    ax_.grid()
+
+p = span_select(grp.datetime, grp.temp_bme, ax[1, 0], fig2)
+
+# %%
+for ax_ in ax.flatten():
+    ax_.axvspan(p.min, p.max, facecolor='grey', alpha=0.2)
+
+df_full.loc[p.selected_x.index, 'quality_flag'] = 1
+fig2.savefig(base_dir + '/Viet/cal_flights/ts_' +
+             str(grp.datetime.min()).replace(' ', '_').replace(':', '-') + '.png', bbox_inches='tight')
+plt.close('all')
+
+# %%
+data_plot = df_full[df_full['quality_flag'] == 1]
 fig, ax = plt.subplots(4, 2, figsize=(12, 9))
 for ax_, x, y in zip(ax.flatten(),
                      ['press_1m', 'temp_1m', 'rh_1m', 'temp_1m', 'rh_1m',
@@ -169,6 +183,7 @@ for ax_, x, y in zip(ax.flatten(),
                color='grey', linewidth=0.5, ls='--')
     ax_.grid()
 fig.subplots_adjust(hspace=0.5)
-fig.savefig(base_dir + r'Viet/cal_flights/compare.png',
+fig.savefig(base_dir + r'Viet/cal_flights/compare_filtered.png',
             bbox_inches='tight')
 # %%
+df_full.to_csv(base_dir + r'Viet/cal_flights/data.csv', index=False)
